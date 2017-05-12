ef07f46e62df15826da432c31ceae2e2b69f766e
LisaTsai <fhdg34@cavedu.com>
2017-05-12 10:07:25 +0800

    20170505

---
+++ recursiveiteration/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDir>
+#include <QFileDialog>
+#include <QFile>
+#include <qdebug.h>
+#include <iostream>
+using namespace std;
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_PathBtn_clicked();
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
